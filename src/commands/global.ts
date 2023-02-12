76f8a31f889c709081007f94359b5db2d120f8b2
pshu <pishu.spf@antfin.com>
2023-02-12 13:45:06 +0800

    feat: ✨ setup global config for polite with *.seatup backup file

---
+++ src/commands/global.ts
@@ -0,0 +1,29 @@
+import { Command } from '@oclif/core';
+import * as fs from 'fs';
+import { exec } from 'shelljs';
+
+export default class Global extends Command {
+  static description = 'describe the command here';
+
+  static examples = ['<%= config.bin %> <%= command.id %>'];
+
+  public async run(): Promise<void> {
+    this.setupGlobalGitignore();
+  }
+
+  private setupGlobalGitignore() {
+    const gitignore = `${process.env.HOME}/.gitignore_global`;
+
+    if (!fs.existsSync(gitignore)) {
+      fs.writeFileSync(gitignore, '*.seatup\n');
+      exec(`git config --global core.excludesfile ${gitignore}`);
+      return;
+    }
+
+    const content = fs.readFileSync(gitignore, 'utf-8');
+    if (!content.includes('*.seatup')) {
+      fs.writeFileSync(gitignore, `${content}\n*.seatup`);
+    }
+    return;
+  }
+}
