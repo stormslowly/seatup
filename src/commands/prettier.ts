e91714d6c7cea863c223a8ac829f07c9b3c44aa0
pshu <pishu.spf@antfin.com>
2023-12-06 21:00:28 +0800

    chore: ⬆️ update prettier version

--- src/commands/prettier.ts
+++ src/commands/prettier.ts
@@ -1,5 +1,5 @@
-import { Command } from '@oclif/core';
-import { assert, NpmProjectUtil, pkgUpSync } from '../utils';
+import {Command} from '@oclif/core';
+import {assert, NpmProjectUtil, pkgUpSync} from '../utils';
 
 export default class Prettier extends Command {
   static description = 'setup prettier basic config';
@@ -13,14 +13,14 @@ export default class Prettier extends Command {
     const projectUtil = new NpmProjectUtil(pkgPath);
 
     projectUtil
-      .addDevDeps('prettier', '^2.8.4')
+      .addDevDeps('prettier', '^3.1.0')
       .addDevDeps('prettier-plugin-organize-imports', '^3.2.2')
       .addDevDeps('prettier-plugin-packagejson', '^2.4.2')
       .writeFileToRoot(
         '.prettierrc.js',
         `
 module.exports = {
-  printWidth: 80,
+  printWidth: 120,
   singleQuote: true,
   trailingComma: "all",
   proseWrap: "never",
